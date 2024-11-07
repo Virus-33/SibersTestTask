bfc2430d6419047e942325a254445211871865a7
Virus-33 <hazhdachka.tv@gmail.com>
2024-11-07 17:20:51 +0700

    initial commit

---
+++ TTask/Models/Person.cs
@@ -0,0 +1,33 @@
+﻿namespace TTask.Models
+{
+    public class Person
+    {
+        public Person(int id, string name, string surname, string patronymic, string email)
+        {
+            Id = id;
+            Name = name;
+            Surname = surname;
+            Patronymic = patronymic;
+            Email = email;
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Patronymic { get; set; }
+
+        public string Email { get; set; }
+
+        public Roles Role { get; set; }
+
+        public enum Roles : int
+        {
+            Employee = 0,
+            Manager = 1,
+            CEO = 2
+        }
+    }
+}
---
+++ TTask/Models/Project.cs
@@ -0,0 +1,37 @@
+﻿namespace TTask.Models
+{
+    public class Project
+    {
+        public Project(int id, string name, string ccompany, string ecompany, DateOnly sdate, DateOnly edate) 
+        {
+            Id = id;
+            Name = name;
+            ClientCompany = ccompany;
+            ExecutorCompany = ecompany;
+            StartingDate = sdate;
+            EndingDate = edate;
+        }
+
+        public Project()
+        {
+            
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string ClientCompany {  get; set; }
+
+        public string ExecutorCompany { get; set; }
+
+        public DateOnly StartingDate { get; set; }
+
+        public DateOnly EndingDate { get; set; }
+        
+        public Person ProjectManager { get; set; }
+
+        public List<Person> People { get; set; }
+
+        public int Priority {  get; set; }
+    }
+}
